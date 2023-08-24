651c5d4e72ad91a79e30d463cc99907828a43d36
Othman <othman.laaziri@verosoftdesign.com>
2023-08-24 10:30:32 -0400

    Adding observ

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  activeComponent: string = ''; // Initialize to an empty string
+export class AppComponent implements OnInit {
+  activeComponent: string = '';
+  defaultText: string = 'Select a button to learn more.';
+  activeComponentSubject: BehaviorSubject<string> = new BehaviorSubject<string>(this.defaultText);
+
+  ngOnInit() {
+    this.activeComponentSubject.next(this.defaultText);
+  }
 
   toggleComponent(component: string) {
     if (this.activeComponent === component) {
-      this.activeComponent = ''; // Toggle off if clicked again
+      this.activeComponent = '';
+      this.activeComponentSubject.next(this.defaultText);
     } else {
       this.activeComponent = component;
+      this.activeComponentSubject.next(component);
     }
   }
 }
